35c98fbde06704b41cf88d75d049e7947ba49a7e
Liam Wood <liamvwood@ufl.edu>
2018-12-07 01:50:07 -0500

    angular includes and initial initialization

--- client/js/app.js
+++ client/js/app.js
@@ -1,2 +1,7 @@
 angular.module('flowers', []);
+angular.module('features', []);
+angular.module('sightings', []);
+angular.module('users', []);
+
+
 var app = angular.module('flowersApp', ['flowers']);
