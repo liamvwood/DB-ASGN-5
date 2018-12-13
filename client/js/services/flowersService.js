07ab025394a4241e75c388a2b2ee14e2d60d2f7d
Liam Wood <liamvwood@ufl.edu>
2018-12-12 23:22:57 -0500

    hiding API key

--- client/js/services/flowersService.js
+++ client/js/services/flowersService.js
@@ -4,8 +4,8 @@ angular.module('flowers').factory('Flowers', function($http) {
             return $http.get('./api/flowers/');
         },
         getUrl: function(name) {
-            let CUSTOM_SEARCH_ID = "002721976270332564454:irkkdg5omgc";
-            let SERVER_KEY = "AIzaSyCJleSyNsxXXnEqtskMIL3tTZv2u8Vechs";
+            let CUSTOM_SEARCH_ID = "INSERT KEY";
+            let SERVER_KEY = "INSET API KEY";
             return $http.get(`https://www.googleapis.com/customsearch/v1?key=${SERVER_KEY}&cx=${CUSTOM_SEARCH_ID}&q=${name}&searchType=image&fileType=jpg&imgSize=xlarge&num=1&alt=json`)
         },
         updateFlower: function(name, data) {
